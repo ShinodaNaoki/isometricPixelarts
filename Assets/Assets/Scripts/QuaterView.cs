ad1e31bf36ceaad1f66f03422c6a5e4160504ef2
Shinoda Naoki <shinoda.naoki@techcross.co.jp>
2019-07-16 07:38:06 +0900

    pixel perfect quater view

---
+++ Assets/Assets/Scripts/QuaterView.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class QuaterView : MonoBehaviour
+{
+    private new Camera camera;
+
+    [SerializeField, Header("カメラが移す中心地")]
+    private Vector3Int lookAt;
+
+    [SerializeField, Header("中心地の画面内上下位置(0～1)")]
+    private float vertFocusPos;
+
+    [SerializeField, Header("カメラを離す距離")]
+    private int distance = 256;
+
+    [SerializeField, Header("ピクセル倍率"), Min(1)]
+    private int zoom = 1;
+
+
+    void Awake()
+    {
+        camera = gameObject.GetComponent<Camera>();
+        Assert.IsNotNull(camera);
+        AdjustCamera();
+    }
+
+    void OnValidate()
+    {
+        Awake();
+    }
+
+    private float CalcOrthoSize()
+    {
+        // 画面の高さの半分＝等倍サイズ
+        return camera.pixelHeight / (zoom * 2f);
+    }
+
+    private void AdjustCamera()
+    {
+        camera.transform.rotation = Quaternion.identity;
+        camera.transform.position = Vector3.zero;
+        camera.ResetProjectionMatrix();
+
+        int depth = (lookAt.x + lookAt.z) / 2;
+        Vector3Int pos = new Vector3Int(lookAt.x - lookAt.z, lookAt.y + depth, lookAt.y - depth);
+
+        var matrix = new Matrix4x4()
+        {
+            m00 = 1.0f, m01 = 0.0f, m02 =-1.0f, m03 = 0.5f - pos.x,
+            m10 = 0.5f, m11 = 1.0f, m12 = 0.5f, m13 = 0.0f - pos.y,
+            m20 =-0.5f, m21 = 1.0f, m22 =-0.5f, m23 = 0.0f - pos.z - distance,
+            m30 = 0.0f, m31 = 0.0f, m32 = 0.0f, m33 = 1.0f            
+        };
+
+        camera.worldToCameraMatrix = matrix;
+        camera.orthographicSize = CalcOrthoSize();
+       
+    }
+}
